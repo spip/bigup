18fd34d98b3d31cbd69cd41f6ced21355d91051a
Salvatore <salvatore@rezo.net>
2021-03-09 01:27:04 +0100

    [Salvatore] [source:lang/ paquet-bigup] Export depuis https://trad.spip.net de la langue en [Salvatore] [source:lang/ paquet-bigup] Export depuis https://trad.spip.net de la langue fr [Salvatore] [source:lang/ paquet-bigup] Mise a jour du bilan depuis https://trad.spip.net

--- lang/paquet-bigup_en.php
+++ lang/paquet-bigup_en.php
@@ -1,12 +1,16 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/bigup-paquet-xml-bigup?lang_cible=en
+// ** ne pas modifier le fichier **
 
-if (!defined('_ECRIRE_INC_VERSION')) return;
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// B
 	'bigup_description' => '',
 	'bigup_nom' => 'Big Upload',
-	'bigup_slogan' => 'Upload files, whatever their size',
+	'bigup_slogan' => 'Upload files, whatever their size'
 );
--- lang/paquet-bigup_fr.php
+++ lang/paquet-bigup_fr.php
@@ -1,12 +1,14 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-
-if (!defined('_ECRIRE_INC_VERSION')) return;
+// Fichier source, a modifier dans https://git.spip.net/spip/bigup.git
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// B
 	'bigup_description' => '',
 	'bigup_nom' => 'Big Upload',
-	'bigup_slogan' => 'Téléverser des fichiers, quelque soit leur taille',
+	'bigup_slogan' => 'Téléverser des fichiers, quelque soit leur taille'
 );
