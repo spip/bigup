cb3f188d44d294a08b584027cb31266a9292328a
Salvatore <salvatore@rezo.net>
2021-03-09 01:27:01 +0100

    [Salvatore] [source:lang/ bigup] Export depuis https://trad.spip.net de la langue en [Salvatore] [source:lang/ bigup] Export depuis https://trad.spip.net de la langue fr [Salvatore] [source:lang/ bigup] Mise a jour du bilan depuis https://trad.spip.net

--- lang/bigup_en.php
+++ lang/bigup_en.php
@@ -1,8 +1,12 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/bigup-bigup?lang_cible=en
+// ** ne pas modifier le fichier **
 
-if (!defined('_ECRIRE_INC_VERSION')) return;
- 
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// B
@@ -11,24 +15,24 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'bouton_enlever' => 'Remove',
 
 	// C
-	'choisir' => 'Choose',
 	'cfg_charger_public' => 'Script in the public website',
 	'cfg_charger_public_case' => 'Load scripts on the public website',
 	'cfg_max_file_size' => 'Maximum size of files',
 	'cfg_max_file_size_explication' => 'Maximum size of files (in Mb).',
 	'cfg_titre_parametrages' => 'Big Upload settings',
+	'choisir' => 'Choose',
 
 	// D
+	'deposer_la_vignette_ici' => 'Drop the thumbnail here',
+	'deposer_le_logo_ici' => 'Drop the LOGO here',
 	'deposer_vos_fichiers_ici' => 'Drop your files here',
 	'deposer_votre_fichier_ici' => 'Drop your file here',
-	'deposer_le_logo_ici' => 'Drop the LOGO here',
-	'deposer_la_vignette_ici' => 'Drop the thumbnail here',
 
 	// E
 	'erreur_de_transfert' => 'Upload error',
+	'erreur_probleme_survenu' => 'Some trouble occured…',
 	'erreur_taille_max' => 'Maximum file size is @taille@',
 	'erreur_type_fichier' => 'File type unallowed!',
-	'erreur_probleme_survenu' => 'Some trouble occured…',
 
 	// F
 	'fichier_efface' => 'File deleted',
@@ -47,5 +51,5 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'titre_page_configurer_bigup' => 'Big Upload Configuration',
 
 	// Z
-	'zbug_necessite_plugin' => 'The <code>@balise@</code> tag needs the plugin <code>@plugin@</code>',
+	'zbug_necessite_plugin' => 'The <code>@balise@</code> tag needs the plugin <code>@plugin@</code>'
 );
--- lang/bigup_fr.php
+++ lang/bigup_fr.php
@@ -1,8 +1,10 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans https://git.spip.net/spip/bigup.git
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
 
-if (!defined('_ECRIRE_INC_VERSION')) return;
- 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// B
@@ -11,24 +13,24 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'bouton_enlever' => 'Enlever',
 
 	// C
-	'choisir' => 'Choisir',
 	'cfg_charger_public' => 'Script dans l’espace public',
 	'cfg_charger_public_case' => 'Charger les scripts dans l’espace public',
 	'cfg_max_file_size' => 'Taille maximum des fichiers',
 	'cfg_max_file_size_explication' => 'Taille maximum des fichiers (en Mb).',
 	'cfg_titre_parametrages' => 'Paramétrages de Big Upload',
+	'choisir' => 'Choisir',
 
 	// D
+	'deposer_la_vignette_ici' => 'Déposer la vignette ici',
+	'deposer_le_logo_ici' => 'Déposer le LOGO ici',
 	'deposer_vos_fichiers_ici' => 'Déposer vos fichiers ici',
 	'deposer_votre_fichier_ici' => 'Déposer votre fichier ici',
-	'deposer_le_logo_ici' => 'Déposer le LOGO ici',
-	'deposer_la_vignette_ici' => 'Déposer la vignette ici',
 
 	// E
 	'erreur_de_transfert' => 'Erreur de transfert.',
-	'erreur_taille_max' => 'Le fichier ne doit pas dépasser @taille@',
-	'erreur_type_fichier' => 'Type de fichier incorrect !',
 	'erreur_probleme_survenu' => 'Un problème est survenu…',
+	'erreur_taille_max' => 'Le fichier ne doit pas dépasser @taille@',
+	'erreur_type_fichier' => 'Type de fichier incorrect !',
 
 	// F
 	'fichier_efface' => 'Fichier effacé',
@@ -47,5 +49,5 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'titre_page_configurer_bigup' => 'Configurer Big Upload',
 
 	// Z
-	'zbug_necessite_plugin' => 'La balise <code>@balise@</code> nécessite le plugin <code>@plugin@</code>',
+	'zbug_necessite_plugin' => 'La balise <code>@balise@</code> nécessite le plugin <code>@plugin@</code>'
 );
