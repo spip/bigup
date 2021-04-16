cc6880d3375af0d4bbc376af3f50c9a56d4069b2
klaus++ <klaus@spip.de>
2021-04-17 01:27:09 +0200

    [Salvatore] [source:lang/ paquet-bigup] Export depuis https://trad.spip.net de la langue de [Salvatore] [source:lang/ paquet-bigup] Mise a jour du bilan depuis https://trad.spip.net

---
+++ lang/paquet-bigup_de.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/bigup-paquet-xml-bigup?lang_cible=de
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'bigup_description' => '... wie der Name schon sagt ...',
+	'bigup_nom' => 'Big Upload',
+	'bigup_slogan' => 'Dateien hochladen, egal wie gross sie sind.'
+);
