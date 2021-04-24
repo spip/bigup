e814f694a04a23815850b097e3956b9eaddf56fa
George <george@middleeastwatch.net>
2021-04-25 01:27:16 +0200

    [Salvatore] [source:lang/ paquet-bigup] Export depuis https://trad.spip.net de la langue ar [Salvatore] [source:lang/ paquet-bigup] Mise a jour du bilan depuis https://trad.spip.net

---
+++ lang/paquet-bigup_ar.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/bigup-paquet-xml-bigup?lang_cible=ar
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'bigup_description' => '',
+	'bigup_nom' => 'تحميل كبير',
+	'bigup_slogan' => 'تحميل الملفات مهما كان حجمها'
+);
