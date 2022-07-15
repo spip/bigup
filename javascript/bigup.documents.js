786ba9fa487b81574a372b64c126e94385dce685
Matthieu Marcillaud <marcimat@rezo.net>
2022-07-15 15:54:38 +0200

    refactor: Les js de bigup utilisent `bigup.buildFormData()` et `bigup.send()`.
    Refs: #4861
    (cherry picked from commit be52959e6c53f0ff213b86a2c515ddb1b2ed59e6)

--- javascript/bigup.documents.js
+++ javascript/bigup.documents.js
@@ -7,48 +7,49 @@ function formulaires_documents_avec_bigup () {
 		.not('.bigup_document')
 		.addClass('bigup_document')
 		.on('bigup.fileSuccess', function(event, file, description) {
-			var bigup = file.bigup;
-			var input = file.emplacement;
+			const bigup = file.bigup;
+			const input = file.emplacement;
 
-			var data = $.extend(bigup.getFormData(), {
-				joindre_upload: true,
-				joindre_zip: true, // les zips sont conservés zippés systématiquement.
-				formulaire_action_verifier_json: true,
-				bigup_reinjecter_uniquement: [description.bigup.identifiant],
-			});
+			const data = bigup.buildFormData();
+			data.set('joindre_upload', true);
+			data.set('joindre_zip', true); // les zips sont conservés zippés systématiquement.
+			data.set('formulaire_action_verifier_json', true);
+			data.set('bigup_reinjecter_uniquement', [description.bigup.identifiant]);
 
 			// verifier les champs
-			$.post(bigup.target, data, null, 'json')
-				.done(function(erreurs) {
-					var erreur = erreurs[bigup.name] || erreurs.message_erreur;
-					if (erreur) {
-						bigup.presenter_erreur(input, erreur);
-					} else {
-						delete data.formulaire_action_verifier_json;
-						// Faire le traitement prévu, supposant qu'il n'y aura pas d'erreur...
-						$.post(bigup.target, data)
-							.done(function(html) {
-								var message = $(html).find('.reponse_formulaire').html();
-								if (message) {
-									bigup.presenter_succes(input, message);
-								} else {
-									bigup.presenter_erreur(input, _T('bigup:erreur_probleme_survenu'));
-								}
-								input.addClass('remove').animate({opacity: "0.0"}, 'fast', function(){
-									// autoriser de mettre une seconde fois le fichier
-									file.bigup_deleted = true;
-									file.cancel();
-									input.remove();
-								});
-							})
-							.fail(function(data) {
-								bigup.presenter_erreur(input, _T('bigup:erreur_probleme_survenu'));
-							});
-					}
-				})
-				.fail(function(data) {
-					bigup.presenter_erreur(input, _T('bigup:erreur_probleme_survenu'));
-				});
+			bigup
+			.send(data, {dataType: 'json'})
+			.done(function(erreurs) {
+				var erreur = erreurs[bigup.name] || erreurs.message_erreur;
+				if (erreur) {
+					bigup.presenter_erreur(input, erreur);
+				} else {
+					data.delete('formulaire_action_verifier_json');
+					// Faire le traitement prévu, supposant qu'il n'y aura pas d'erreur...
+					bigup
+					.send(data)
+					.done(function(html) {
+						var message = $(html).find('.reponse_formulaire').html();
+						if (message) {
+							bigup.presenter_succes(input, message);
+						} else {
+							bigup.presenter_erreur(input, _T('bigup:erreur_probleme_survenu'));
+						}
+						input.addClass('remove').animate({opacity: "0.0"}, 'fast', function(){
+							// autoriser de mettre une seconde fois le fichier
+							file.bigup_deleted = true;
+							file.cancel();
+							input.remove();
+						});
+					})
+					.fail(function(data) {
+						bigup.presenter_erreur(input, _T('bigup:erreur_probleme_survenu'));
+					});
+				}
+			})
+			.fail(function(data) {
+				bigup.presenter_erreur(input, _T('bigup:erreur_probleme_survenu'));
+			});
 		});
 	// Si les inputs d'upload sont tout seuls dans un .boutons, cacher ce dernier, sinon juste l'input
 	$("input[name='joindre_upload']").each(function() {
--- javascript/bigup.documents_illustrer.js
+++ javascript/bigup.documents_illustrer.js
@@ -8,41 +8,42 @@ function formulaires_documents_illustrer_avec_bigup () {
 		.not('.bigup_done')
 		.bigup()
 		.on('bigup.fileSuccess', function(event, file, description) {
-			var bigup = file.bigup;
-			var input = file.emplacement;
+			const bigup = file.bigup;
+			const input = file.emplacement;
 
-			var data = $.extend(bigup.getFormData(), {
-				joindre_upload: true,
-				joindre_zip: true, // les zips sont conservés zippés systématiquement.
-				formulaire_action_verifier_json: true,
-				bigup_reinjecter_uniquement: [description.bigup.identifiant],
-			});
+			const data = bigup.buildFormData();
+			data.set('joindre_upload', true);
+			data.set('joindre_zip', true); // les zips sont conservés zippés systématiquement.
+			data.set('formulaire_action_verifier_json', true);
+			data.set('bigup_reinjecter_uniquement', [description.bigup.identifiant]);
 
 			// verifier les champs
-			$.post(bigup.target, data, null, 'json')
-				.done(function(erreurs) {
-					var erreur = erreurs[bigup.name] || erreurs.message_erreur;
-					if (erreur) {
-						bigup.presenter_erreur(input, erreur);
-					} else {
-						delete data.formulaire_action_verifier_json;
-						var conteneur = bigup.form.parents('.formulaire_illustrer_documents');
-						conteneur.animateLoading();
-						// Faire le traitement prévu, supposant qu'il n'y aura pas d'erreur...
-						$.post(bigup.target, data)
-							.done(function(html) {
-								bigup.presenter_succes(input, _T('bigup:succes_vignette_envoyee'));
-								bigup.form.parents('.formulaire_spip').parent().html(html);
-							})
-							.fail(function(data) {
-								conteneur.endLoading();
-								bigup.presenter_erreur(input, _T('bigup:erreur_probleme_survenu'));
-							});
-					}
-				})
-				.fail(function(data) {
-					bigup.presenter_erreur(input, _T('bigup:erreur_probleme_survenu'));
-				});
+			bigup
+			.send(data, {dataType: 'json'})
+			.done(function(erreurs) {
+				var erreur = erreurs[bigup.name] || erreurs.message_erreur;
+				if (erreur) {
+					bigup.presenter_erreur(input, erreur);
+				} else {
+					data.delete('formulaire_action_verifier_json');
+					var conteneur = bigup.form.parents('.formulaire_illustrer_documents');
+					conteneur.animateLoading();
+					// Faire le traitement prévu, supposant qu'il n'y aura pas d'erreur...
+					bigup
+					.send(data)
+					.done(function(html) {
+						bigup.presenter_succes(input, _T('bigup:succes_vignette_envoyee'));
+						bigup.form.parents('.formulaire_spip').parent().html(html);
+					})
+					.fail(function(data) {
+						conteneur.endLoading();
+						bigup.presenter_erreur(input, _T('bigup:erreur_probleme_survenu'));
+					});
+				}
+			})
+			.fail(function(data) {
+				bigup.presenter_erreur(input, _T('bigup:erreur_probleme_survenu'));
+			});
 		})
 		.closest('.editer').find('.dropfiletext').html(_T('bigup:deposer_la_vignette_ici'));
 }
