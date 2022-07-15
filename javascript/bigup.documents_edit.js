b9f802109435ed920447b91da38fef66f3a90939
Matthieu Marcillaud <marcimat@rezo.net>
2022-07-15 14:40:54 +0200

    fix: Ajout d’une méthode `buildFormData()` qui supplante `getFormData()`
    Cette fonction retourne un objet `FormData` plutôt qu’un tableau d’une part,
    et d’autre part, elle fonctionne mieux lorsque le name accepte plusieurs entrées
    tel que `name="items[]"`.
    
    Cela oblige néanmoins à réécrire les scripts qui utilisaient la sortie de `getFormData()`
    avec les méthodes natives de `FormData` tel que `.set()`, `.append()` ou `.delete()`
    d’une part, et aussi de transmettre l’option `processData: false,` à jQuery.ajax
    
    Refs: #4861
    (cherry picked from commit bb20c8df111a0c89038fe897c023e9b16664f294)

--- javascript/bigup.documents_edit.js
+++ javascript/bigup.documents_edit.js
@@ -9,42 +9,57 @@ function formulaires_documents_edit_avec_bigup () {
 		.not('.bigup_document_edit')
 		.addClass('bigup_document_edit')
 		.on('bigup.fileSuccess', function(event, file, description) {
-			var bigup = file.bigup;
-			var input = file.emplacement;
+			const bigup = file.bigup;
+			const input = file.emplacement;
 
-			var data = $.extend(bigup.getFormData(), {
-				joindre_upload: true,
-				joindre_zip: true, // les zips sont conservés zippés systématiquement.
-				formulaire_action_verifier_json: true,
-				bigup_reinjecter_uniquement: [description.bigup.identifiant],
-			});
+			const data = bigup.buildFormData();
+			data.set('joindre_upload', true);
+			data.set('joindre_zip', true);
+			data.set('formulaire_action_verifier_json', true);
+			data.set('bigup_reinjecter_uniquement', [description.bigup.identifiant]);
 
 			// verifier les champs
-			$.post(bigup.target, data, null, 'json')
-				.done(function(erreurs) {
-					var erreur = data.fichier_upload || erreurs.message_erreur;
-					if (erreur) {
-						bigup.presenter_erreur(input, erreur);
-					} else {
-						delete data.formulaire_action_verifier_json;
-						// Faire le traitement prévu, supposant qu'il n'y aura pas d'erreur...
-						var conteneur = bigup.form.parents('.formulaire_editer_document');
-						conteneur.animateLoading();
-						// Faire le traitement prévu, supposant qu'il n'y aura pas d'erreur...
-						$.post(bigup.target, data)
-							.done(function(html) {
-								bigup.presenter_succes(input, _T('bigup:succes_fichier_envoye'));
-								bigup.form.parents('.formulaire_spip').parent().html(html);
-							})
-							.fail(function(data) {
-								conteneur.endLoading();
-								bigup.presenter_erreur(input, _T('bigup:erreur_probleme_survenu'));
-							});
-					}
-				})
-				.fail(function(data) {
-					bigup.presenter_erreur(input, _T('bigup:erreur_probleme_survenu'));
-				});
+			$.ajax({
+				type: "POST",
+				url: bigup.target,
+				data: data,
+				processData: false,
+				contentType: false,
+				cache: false,
+				dataType: 'json',
+			})
+			.done(function(erreurs) {
+				var erreur = erreurs[bigup.name] || erreurs.message_erreur;
+				if (erreur) {
+					bigup.presenter_erreur(input, erreur);
+				} else {
+					data.delete('formulaire_action_verifier_json');
+
+					// Faire le traitement prévu, supposant qu'il n'y aura pas d'erreur...
+					var conteneur = bigup.form.parents('.formulaire_editer_document');
+					conteneur.animateLoading();
+					// Faire le traitement prévu, supposant qu'il n'y aura pas d'erreur...
+					$.ajax({
+						type: "POST",
+						url: bigup.target,
+						data: data,
+						processData: false,
+						contentType: false,
+						cache: false,
+					})
+					.done(function(html) {
+						bigup.presenter_succes(input, _T('bigup:succes_fichier_envoye'));
+						bigup.form.parents('.formulaire_spip').parent().html(html);
+					})
+					.fail(function(data) {
+						conteneur.endLoading();
+						bigup.presenter_erreur(input, _T('bigup:erreur_probleme_survenu'));
+					});
+				}
+			})
+			.fail(function(data) {
+				bigup.presenter_erreur(input, _T('bigup:erreur_probleme_survenu'));
+			});
 		});
 }
 jQuery(function($) {
